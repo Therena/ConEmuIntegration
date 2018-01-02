376f00376711eae4ddb07a4e4c76c68563e8c2a2
David Roller <david-roller@outlook.com>
2018-01-02 19:31:14 +0100

    Updated the copyright Updated the dependencies

--- ConEmuIntegration/ToolWindow/ConEmuWindowCommand.cs
+++ ConEmuIntegration/ToolWindow/ConEmuWindowCommand.cs
@@ -1,5 +1,5 @@
 ﻿//
-// Copyright 2017 David Roller
+// Copyright 2018 David Roller
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
